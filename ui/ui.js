5bf6b42ae543891f80c7161535b1ffc2e4338d97
Felix Geller <fgeller@gmail.com>
2015-08-17 21:31:33 -0500

    blank page in iframe on startup.

--- ui/ui.js
+++ ui/ui.js
@@ -47,7 +47,7 @@ var Search = React.createClass({
                   <SearchField query={this.state.query} search={this.search}/>
                   <div>{entries}</div>
                   <div>
-                    <iframe id="ifrm" src="/" style={{width:'1000px',height:'400px'}} />
+                    <iframe id="ifrm" src="" style={{width:'1000px',height:'400px'}} />
                   </div>
                 </div>
         );

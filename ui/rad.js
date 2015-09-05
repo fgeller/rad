93deb8baa08723a7685869b3d9a0e65547a72cca
Felix Geller <fgeller@gmail.com>
2015-09-05 23:44:46 +1200

    colorize first search result.

--- ui/rad.js
+++ ui/rad.js
@@ -33,7 +33,11 @@ var SearchResult = React.createClass({
     render: function() {
         var entName = this.props.entry["Entity"];
         var funName = this.props.entry["Function"] || "\u00a0"; // &nbsp;
-        return <div className="search-result" onClick={this.open}>
+        var clsName = "search-result"
+        if (this.props.index == 0) {
+            clsName += " first-search-result"
+        }
+        return <div className={clsName} onClick={this.open}>
                  <div className="entity-name">{entName}</div>
                  <div className="function-name">{funName}</div>
                </div>

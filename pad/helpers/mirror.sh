8da4c84e1010f2a3e81823870bcee2d586cfd36a
Felix Geller <fgeller@gmail.com>
2015-10-03 21:48:38 +1300

    pad: Adds helper script used to generate go pack (fixes #9).

---
+++ pad/helpers/mirror.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+PFX=godoc
+
+# --wait: don't spam the host
+# -E: convert extensions, adds .html/.css
+# -k: convert links to relative ones
+# -H: allow spanning hosts
+# -D: include the following list of domains
+# -D: exclude the listed directories
+# -m: mirror the given site
+# -l: limit the recursion level
+# -P: local directory prefix for downloaded files
+wget --wait=0.5 \
+     -E \
+     -k \
+     -H \
+     -D'godoc.org,maxcdn.bootstrapcdn.com,ajax.googleapis.com' \
+     -m \
+     -l1 \
+     -P$PFX \
+     http://godoc.org/-/go
+
+# don't show page header and footer
+echo ".navbar-default, #x-footer, #x-pkginfo { display: none; }" >> `find $PFX -name "site.css*"`
+
+rm -v $PFX/godoc.org/-/index.html

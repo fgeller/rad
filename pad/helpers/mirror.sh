89b1cf4cb6fe993629d77c96a570562f1edf5620
Felix Geller <fgeller@gmail.com>
2015-10-04 16:51:35 +1300

    pad: Tweaks go doc mirror script.

--- pad/helpers/mirror.sh
+++ pad/helpers/mirror.sh
@@ -2,6 +2,7 @@
 set -e
 
 PFX=godoc
+PKG=http://godoc.org/github.com/gorilla/websocket
 
 # --wait: don't spam the host
 # -E: convert extensions, adds .html/.css
@@ -20,9 +21,14 @@ wget --wait=0.5 \
      -m \
      -l1 \
      -P$PFX \
-     http://godoc.org/-/go
+     $PKG
+
+# disable google analytics
+find . -name "*.html" -exec sed -i -e 's:type="text/javascript":type="text/disabled":g' {} \;
 
 # don't show page header and footer
-echo ".navbar-default, #x-footer, #x-pkginfo p { display: none; }" >> `find $PFX -name "site.css*"`
+echo ".navbar-default, #x-footer { display: none; }" >> `find $PFX -name "site.css*"`
+echo "#x-pkginfo { visibility: hidden; }" >> `find $PFX -name "site.css*"`
 
+# for some reason -X / exclude does not work
 rm -v $PFX/godoc.org/-/index.html

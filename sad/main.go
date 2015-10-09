3e6589f88c016b2ffb4a0fe2297071dccc2ac802
Felix Geller <fgeller@gmail.com>
2015-10-09 20:34:00 +1300

    sad: Initializes global state in main.

--- sad/main.go
+++ sad/main.go
@@ -20,6 +20,8 @@ func main() {
 	flag.StringVar(&config.sapAddr, "sapaddr", "localhost:3025", "Addr where sap is running")
 	flag.Parse()
 
+	global.packs = map[string]shared.Pack{}
+	global.docs = map[string][]shared.Namespace{}
 	loadInstalled()
 	serve("0.0.0.0:3024")
 }

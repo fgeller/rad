9b04911fc147e16b4a35a3ef6c2e5db51c1aef35
Felix Geller <fgeller@gmail.com>
2015-10-19 07:59:20 +1300

    pad: Adds a mirror script for jquery (for #45).

---
+++ pad/helpers/jqy-mirror.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PFX=jquery
+PKG=http://api.jquery.com/
+
+# --random-wait
+# --wait: don't spam the host
+# -E: convert extensions, adds .html/.css
+# -k: convert links to relative ones
+# -H: allow spanning hosts
+# -D: include the following list of domains
+# -m: mirror the given site
+# -l: limit the recursion level
+# -P: local directory prefix for downloaded files
+wget --wait=0.5 \
+     -E \
+     -e robots=off \
+     -k \
+     -H \
+     -D api.jquery.com,use.typekit.net,ajax.googleapis.com \
+     -m \
+     -l 1 \
+     -P$PFX \
+     $PKG
+
+mkdir -p $PFX/api.jquery.com/jquery-wp-content/themes/jquery/css/fonts
+pushd $PFX/api.jquery.com/jquery-wp-content/themes/jquery/css/fonts && wget http://api.jquery.com/jquery-wp-content/themes/jquery/css/fonts/fontawesome-webfont.eot && popd
+pushd $PFX/api.jquery.com/jquery-wp-content/themes/jquery/css/fonts && wget http://api.jquery.com/jquery-wp-content/themes/jquery/css/fonts/fontawesome-webfont.ttf && popd
+pushd $PFX/api.jquery.com/jquery-wp-content/themes/jquery/css/fonts && wget http://api.jquery.com/jquery-wp-content/themes/jquery/css/fonts/fontawesome-webfont.woff && popd
+
+find $PFX/api.jquery.com/jquery-wp-content/themes/jquery/css -name 'base.css*' -exec sed -iE 's$http://api.jquery.com$../../../../../api.jquery.com$g' {} \;

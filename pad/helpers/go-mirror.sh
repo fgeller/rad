0ed6e08a626ea846fc5515c3d086f0eac01ff457
Felix Geller <fgeller@gmail.com>
2015-10-25 10:17:51 +1300

    pad: Tweaks godoc mirror script to include pack description (#52).

--- pad/helpers/go-mirror.sh
+++ pad/helpers/go-mirror.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# PFX=go-stdlib
-# PKG=https://godoc.org/-/go
-PFX=gorilla-websocket
-PKG=http://godoc.org/github.com/gorilla/websocket
+
+PACK_NAME=go-stdlib
+SOURCE=https://godoc.org/-/go
+# PACK_NAME=gorilla-websocket
+# SOURCE=http://godoc.org/github.com/gorilla/websocket
+DOWNLOAD_DIR=d/$PACK_NAME
+
+rm -vrf $DOWNLOAD_DIR
+mkdir -p $DOWNLOAD_DIR
 
 # --random-wait
 # --wait: don't spam the host
@@ -21,16 +26,27 @@ wget --random-wait \
      -D'godoc.org,maxcdn.bootstrapcdn.com,ajax.googleapis.com' \
      -m \
      -l1 \
-     -P$PFX \
-     $PKG
+     -P$DOWNLOAD_DIR \
+     $SOURCE
 
 echo "Disabling Google analytics"
-find $PFX -name "*.html" -exec sed -i -E 's:type="text/javascript":type="text/disabled":g' {} \;
+find $DOWNLOAD_DIR -name "*.html" -exec sed -i -E 's:type="text/javascript":type="text/disabled":g' {} \;
 
 echo "Hiding header and footer"
-echo ".navbar-default, #x-footer { display: none; }" >> `find $PFX -name "site.css*"`
-echo "#x-pkginfo { visibility: hidden; }" >> `find $PFX -name "site.css*"`
+echo ".navbar-default, #x-footer { display: none; }" >> `find $DOWNLOAD_DIR -name "site.css*"`
+echo "#x-pkginfo { visibility: hidden; }" >> `find $DOWNLOAD_DIR -name "site.css*"`
 
 # for some reason -X / exclude does not work
 echo "Manually deleting huge package index."
-rm -fv $PFX/godoc.org/-/index.html
+rm -fv $DOWNLOAD_DIR/godoc.org/-/index.html
+
+read -r -d '' PACK_DESC << EOM
+The contents of <a href="http://golang.org">golang.org</a> is licensed under the Creative Commons Attribution 3.0 License.<br />
+More details on the page and <a href="https://developers.google.com/site-policies#restrictions">here</a>.<br />
+Go itself is distributed with a <a href="https://golang.org/LICENSE">BSD-style license</a>.<br /><br />
+Mirrored from <a href="$SOURCE">$SOURCE</a>.<br />
+Pack created on `date`.<br />
+<br />
+EOM
+
+../pad -indexer go -name $PACK_NAME -desc "$PACK_DESC" -version `date --iso` -source $DOWNLOAD_DIR
